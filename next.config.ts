d80ce278c3aac65287770a629abf825b0c8a7962
kkoreng <chr54858@gmail.com>
2026-05-07 17:55:32 -0500

    feat: 기타 등등..

--- next.config.ts
+++ next.config.ts
@@ -3,31 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     images: { unoptimized: true },
     trailingSlash: true,
-
-    async redirects() {
-        return [
-            {
-                source: "/discord",
-                destination: "https://discord.gg/NanAFkRdww",
-                permanent: false,
-            },
-            {
-                source: "/github",
-                destination: "https://github.com/TeamArcadia",
-                permanent: false,
-            },
-            {
-                source: "/youtube",
-                destination: "https://www.youtube.com/@Arcadia-Team",
-                permanent: false,
-            },
-            {
-                source: "/kakaotalk",
-                destination: "https://pf.kakao.com/_xczgxbX",
-                permanent: false,
-            },
-        ];
-    },
 };
 
 export default nextConfig;
